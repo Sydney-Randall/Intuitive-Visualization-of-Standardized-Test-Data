beb1824961ffede65a6f672af1649ae21469af43
Sydney Randall <sydney.randall@wallawalla.edu>
2017-05-09 14:46:52 -0700

    Nolan: fix graph line and y-axis range
    Change by Nolan on Gitlab; change line to not be filled in & set y-axis to range 0 to 100%

--- seniorProject/src/app/graph.component.ts
+++ seniorProject/src/app/graph.component.ts
@@ -1,3 +1,5 @@
+
+
 import { Component, Input, OnInit, OnChanges, ViewChild, ElementRef, AfterViewChecked} from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Teacher, TEACHERS }  from './teacher';
@@ -21,14 +23,14 @@ export class GraphComponent implements OnInit, OnChanges, AfterViewChecked {
     currentStudent: Student;
     id: string;
 
-  private margin = {top: 20, right: 20, bottom: 30, left: 50};
-  private width: number;
-  private height: number;
-  private x: any;
-  private y: any;
-  private svg: any;
-  private line: d3.Line<[number, number]>;
-  private data: any[];
+    private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    private width: number;
+    private height: number;
+    private x: any;
+    private y: any;
+    private svg: any;
+    private line: d3.Line<[number, number]>;
+    private data: any[];
 
     constructor(
         private route: ActivatedRoute,
@@ -39,83 +41,86 @@ export class GraphComponent implements OnInit, OnChanges, AfterViewChecked {
         });
         this.width = 900 - this.margin.left - this.margin.right;
         this.height = 500 - this.margin.top - this.margin.bottom;
-        for(let test of this.tests) {
+        /*for(let test of this.tests) {
           let taken = this.convertDate(test);
           let percentage = this.convertPercent(test);
           console.log("hey");
           console.log(taken);
-        }
+        }*/
     }
 
     ngOnInit() {
-      this.currentStudent = this.service.getStudent(this.id);
-      this.initSvg();
-      
+        this.currentStudent = this.service.getStudent(this.id);
+        this.initSvg();
+
     }
 
     ngOnChanges() {
     }
 
     ngAfterViewChecked() {
-      this.initAxis();
-      this.drawAxis();
-      this.drawLine();
+        this.initAxis();
+        this.drawAxis();
+        this.drawLine();
     }
     private convertDate(d: Test): Date {
-      let parts = d.taken.split('/');
-      let returnDate = new Date(+parts[2],+parts[0]-1, +parts[1]);
-      console.log(returnDate);
-      return returnDate;
+        let parts = d.taken.split('/');
+        let returnDate = new Date(+parts[2], +parts[0] - 1, +parts[1]);
+        console.log(returnDate);
+        return returnDate;
     }
 
     private convertPercent(d: Test): number {
-      return +d.percentage;
+        return +d.percentage;
     }
 
     private initSvg() {
-    this.svg = d3.select("svg")
-                 .append("g")
-                 .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
-  }
+        this.svg = d3.select("svg")
+            .append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
+    }
 
-  private initAxis() {
-    this.x = d3.scaleTime().range([0, this.width]);
-    this.y = d3.scaleLinear().range([this.height, 0]);
-    this.x.domain(d3.extent(this.tests, this.convertDate));
-    this.y.domain(d3.extent(this.tests, this.convertPercent));
-  }
+    private initAxis() {
+        this.x = d3.scaleTime().range([0, this.width]);
+        this.y = d3.scaleLinear().range([this.height, 0]);
+        this.x.domain(d3.extent(this.tests, this.convertDate));
+        this.y.domain(d3.extent([0, 100]));
+    }
 
-  private drawAxis() {
+    private drawAxis() {
 
-    this.svg.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + this.height + ")")
-          .call(d3.axisBottom(this.x));
+        this.svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(d3.axisBottom(this.x));
 
-    this.svg.append("g")
-          .attr("class", "axis axis--y")
-          .call(d3.axisLeft(this.y))
-          .append("text")
-          .attr("class", "axis-title")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Percentage (%)");
-  }
+        this.svg.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(this.y))
+            .append("text")
+            .attr("class", "axis-title")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Percentage (%)");
+    }
 
-  private drawLine() {
-    this.line = d3.line()
-                       .x( (d: any) => { 
-                         return this.x(this.convertDate(d));
-                       })
-                       .y( (d: any) => { 
-                         return this.y(this.convertPercent(d));
-                       });
+    private drawLine() {
+        this.line = d3.line()
+            .x((d: any) => {
+                return this.x(this.convertDate(d));
+            })
+            .y((d: any) => {
+                return this.y(this.convertPercent(d));
+            });
 
-    this.svg.append("path")
+        this.svg.append("path")
             .datum(this.tests)
             .attr("class", "line")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
             .attr("d", this.line);
-  }
+    }
 }
+
